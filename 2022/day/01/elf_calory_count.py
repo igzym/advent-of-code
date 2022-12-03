580d823cd41640a7fc1d07cbb6a168a848b18c80
igzym <igor.milos@gmail.com>
2022-12-03 11:34:03 +0100

    top 3 elves

--- 2022/day/01/elf_calory_count.py
+++ 2022/day/01/elf_calory_count.py
@@ -1,15 +1,13 @@
 import sys
 
 input_file = sys.argv[1]
-#print(input_file)
 
 with open(input_file) as f:
     lines = f.readlines()
     
-#print('first 10 lines')
-#print(lines[:10])
+# keep track of all elves calories
+elf_cal = []
 
-max_elf = 0
 cur_elf = 0
 
 for ll in lines:
@@ -17,13 +15,35 @@ for ll in lines:
         n = int(ll)
         cur_elf += n
     except ValueError:
-        max_elf = max(max_elf, cur_elf)
+        elf_cal.append(cur_elf)
         cur_elf = 0
-    #print(f'cur_elf {cur_elf} max_elf {max_elf}')
+    #print(f'cur_elf {cur_elf}')
 
 
 # handle the last one
-max_elf = max(max_elf, cur_elf)
-#print(f'cur_elf {cur_elf} max_elf {max_elf}')
+if cur_elf > 0:
+    elf_cal.append(cur_elf)
+    cur_elf = 0
+
+n_elf = len(elf_cal)
+
+top_elf = sorted(range(n_elf), key=lambda i: elf_cal[i], reverse=True)
+
+max_elf = elf_cal[top_elf[0]]
 
 print(f"RESULT: max_elf calories: {max_elf}")
+
+
+top_N = 3
+
+N_to_display = 5
+
+print('top elf ids')
+print(top_elf[:N_to_display])
+print('top elf weights')
+print([elf_cal[i] for i in top_elf[:N_to_display]])
+
+top_N_cal = sum([elf_cal[i] for i in top_elf[:top_N]])
+
+print(f"RESULT: total calories in top {top_N} elves: {top_N_cal}")
+
