faff3f027232391dc733882e4be7a6953d274f93
igzym <igor.milos@gmail.com>
2022-12-07 22:37:32 +0100

    D7 no space left

---
+++ 2022/D07/README.md
@@ -0,0 +1,6 @@
+# Day 7: No Space Left On Device
+
+https://adventofcode.com/2022/day/7
+
+- Input: commands for directory traversal
+- Part 1 answer: sum of sizes of all dires whose size is at most 100000
---
+++ 2022/D07/file_cleaning.py
@@ -0,0 +1,89 @@
+import sys
+
+import re
+
+input_file = sys.argv[1]
+
+with open(input_file) as f:
+    lines = f.readlines()
+
+
+class DirNode:
+    def __init__(self, name, parent):
+        self.name = name
+        self.parent = parent
+        if self.parent is None:
+            self.path = name
+        else:
+            pp = self.parent.path
+            if pp.endswith('/'):
+                pp = pp[:-1]
+            self.path = pp + '/' + name # convenience
+        self.children = []
+        self.file = []
+        self.size = 0
+        # print(f"DEBUG node: {self.path} (new)")
+
+    def add_child_dir(self, node):
+        self.children.append(node)
+        # print(f"DEBUG node: {self.path} new child_dir {node.path}")
+
+    def add_file(self, name, size):
+        self.file.append((name, size))
+        # print(f"DEBUG ... {self.file}")
+
+max_size_threshold = 100000
+tot_size_to_threshold = 0
+
+def calc_sizes(node):
+    global tot_size_to_threshold
+    size = 0
+    for c in node.children:
+        size += calc_sizes(c)
+    for f in node.file:
+        size += f[1]
+    node.size = size
+    # print(f"DEBUG {node.path} size {node.size}")
+    if size <= max_size_threshold:
+        tot_size_to_threshold += size
+    return size
+
+cur_node = None
+root_node = None
+
+ls_req = False
+cd_target = None
+
+for ll in lines:
+    ll = ll.strip("\n")
+
+    ll = ll.split()
+    if ll[0] == '$':
+        if ll[1] == 'cd':
+            ls_req = False
+            cd_target = ll[2]
+            if cd_target == '..':
+                cur_node = cur_node.parent
+                continue
+            if cur_node is None:
+                cur_node = DirNode(cd_target, None)
+                root_node = cur_node
+            else:
+                nn = DirNode(cd_target, cur_node)
+                cur_node.add_child_dir(nn)
+                cur_node = nn
+        elif ll[1] == 'ls':
+            ls_req = True
+            cd_target = None
+        else:
+            assert False, 'unexpected command'
+    else:
+        if ls_req:
+            if ll[0] == 'dir':
+                pass # we don't care about directories...
+            else:
+                cur_node.add_file(ll[1], int(ll[0]))
+
+calc_sizes(root_node) 
+
+print(f"RESULT: tot_size_to_threshold {tot_size_to_threshold}")
