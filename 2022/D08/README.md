10df4b1413f313d788e7c3b9c89ab3500c2aa6b8
igzym <igor.milos@gmail.com>
2022-12-08 21:02:32 +0100

    D8 pt1 nb of visible trees

---
+++ 2022/D08/README.md
@@ -0,0 +1,6 @@
+# Day 8: Treetop Tree House
+
+https://adventofcode.com/2022/day/8
+
+- Input: matrix of tree hights
+- Part 1 answer: number of trees that can be 'seen' from outside
---
+++ 2022/D08/treetop.py
@@ -0,0 +1,77 @@
+import sys
+
+import re
+
+input_file = sys.argv[1]
+
+with open(input_file) as f:
+    lines = f.readlines()
+
+tree_mtx = []
+
+for ll in lines:
+    ll = ll.strip("\n")
+    # print(f"DEBUG {ll}")
+
+    tree_mtx.append([])
+
+    for col in ll:
+        tree_mtx[-1].append(int(col))
+
+# print(tree_mtx)
+
+M = len(tree_mtx)
+N = len(tree_mtx[0])
+
+def visible(tree_mtx, i, j):
+    if i == 0 or j == 0:
+        # print(f"DEBUG visible {i},{j} (left or upper edge)")
+        return True
+    if i == M-1 or j == N-1:
+        # print(f"DEBUG visible {i},{j} (right or bottom edge)")
+        return True
+
+    visible = False
+
+    # scan row (j dimension) to the 'left'
+    max_h = 0
+    for jj in range(0, j):
+        if tree_mtx[i][jj] > max_h:
+            max_h = tree_mtx[i][jj]
+    if max_h < tree_mtx[i][j]:
+        return True
+
+    # scan row (j dimension) to the 'right'
+    max_h = 0
+    for jj in range(j+1, N):
+        if tree_mtx[i][jj] > max_h:
+            max_h = tree_mtx[i][jj]
+    if max_h < tree_mtx[i][j]:
+        return True
+
+    # scan column (i dimension) from 'above'
+    max_h = 0
+    for ii in range(0, i):
+        if tree_mtx[ii][j] > max_h:
+            max_h = tree_mtx[ii][j]
+    if max_h < tree_mtx[i][j]:
+        return True
+
+    # scan column (i dimension) from 'below'
+    max_h = 0
+    for ii in range(i+1, M):
+        if tree_mtx[ii][j] > max_h:
+            max_h = tree_mtx[ii][j]
+    if max_h < tree_mtx[i][j]:
+        return True
+
+    return False
+
+n_visible = 0
+for i in range(M):
+    for j in range(N):
+        if visible(tree_mtx, i, j):
+            
+            n_visible += 1
+
+print(f"RESULT: n_visible {n_visible}")
