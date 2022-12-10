f863bffd53841fb242e5b21c9d73f1a5fb171be4
igzym <igor.milos@gmail.com>
2022-12-10 14:26:31 +0100

    D9 pt 2 - longer rope

--- 2022/D09/README.md
+++ 2022/D09/README.md
@@ -5,3 +5,4 @@ https://adventofcode.com/2022/day/9
 - Input: Series of motions of head knot of a rope
 - We have some rules on how the tail must move in response to had movement
 - Part 1 answer: number of positions that the tail visited
+- Part 2 answer: same thing, but the rope now has 10 knots
--- 2022/D09/rope_bridge.py
+++ 2022/D09/rope_bridge.py
@@ -9,8 +9,7 @@ with open(input_file) as f:
 
 moves = []
 
-head_pos = [0, 0]
-tail_pos = [0, 0]
+knot_pos = [ [0, 0] for _ in range(10) ]
 
 unique_tail_pos = set()
 
@@ -25,11 +24,14 @@ def sign(x):
     else:
         return 0
 
-
-def update_tail_pos(head_pos, tail_pos):
+# update position of knot i based on state of previous knot, i-1
+def update_knot_pos(knot_pos, i):
     global unique_tail_pos
 
-    rvec = [x - y for x, y in zip(head_pos, tail_pos)]
+    head = knot_pos[i]
+    tail = knot_pos[i-1]
+
+    rvec = [x - y for x, y in zip(head, tail)]
 
     rvec_hor, rvec_ver = rvec[0], rvec[1]
 
@@ -39,13 +41,14 @@ def update_tail_pos(head_pos, tail_pos):
 
     if rvec_mod -  sqr2 > 0.001:
         # not touching, need to move the tail
-        tail_pos[0] += sign(rvec_hor)
-        tail_pos[1] += sign(rvec_ver)
+        tail[0] += sign(rvec_hor)
+        tail[1] += sign(rvec_ver)
 
     # print(f"DEBUG ... after move tail {tail_pos}")
 
-
-    unique_tail_pos.add(tuple(tail_pos))
+    if i-1 == 0:
+        # this is the tail of the entire rope
+        unique_tail_pos.add(tuple(tail))
 
 for ll in lines:
     ll = ll.strip("\n")
@@ -54,25 +57,30 @@ for ll in lines:
 
     moves.append([direction, size])
 
+def move_vector(direction):
+    if direction == 'L':
+        return [-1, 0]
+    elif direction == 'R':
+        return [1, 0]
+    elif direction == 'U':
+        return [0, 1]
+    elif direction == 'D':
+        return [0, -1]
+    else:
+        assert False, f"Unexpected direction {direction!r}"
+
+head_pos = knot_pos[-1]
+
 for direction, size in moves:
     # print(f"DEBUG move {direction, size}")
-    if direction == 'L':
-        for m in range(size):
-            head_pos[0] -= 1
-            update_tail_pos(head_pos, tail_pos)
-    elif direction == 'R':
-        for m in range(size):
-            head_pos[0] += 1
-            update_tail_pos(head_pos, tail_pos)
-    elif direction == 'U':
-        for m in range(size):
-            head_pos[1] += 1
-            update_tail_pos(head_pos, tail_pos)
-    elif direction == 'D':
-        for m in range(size):
-            head_pos[1] -= 1
-            update_tail_pos(head_pos, tail_pos)
-    else:
-        assert False, f"unexpected direction {direction}"
+    move_vec = move_vector(direction)
+    for _ in range(size):
+        # make the move incrementally
+        # update head
+        head_pos[0] += move_vec[0]
+        head_pos[1] += move_vec[1]
+        # propagate to nodes previous to it
+        for kidx in list(range(len(knot_pos)-1, 0, -1)):
+            update_knot_pos(knot_pos, kidx)
 
 print(f"RESULT part 1: {len(unique_tail_pos)} unique tail locations")
