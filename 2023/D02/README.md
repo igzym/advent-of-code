254c567a70e1be719ac9f400ec15ea9017436afd
igzym <igor.milos@gmail.com>
2023-12-04 11:29:19 +0100

    D02 cube conundrum part 1 solved

---
+++ 2023/D02/README.md
@@ -0,0 +1,26 @@
+D02 Cube Conundrum
+==================
+
+https://adventofcode.com/2023/day/2
+
+Run as
+
+    python cube_conundrum.py input.txt 1 # for part 1 answer
+    python cube_conundrum.py input.txt 1 # for part 2 answer
+
+Rules
+-----
+
+- In each game Elf hides a certain set of bag of cubes of 3 colours, red, green
+  or blue
+- Elf makes several random draws from the bag and shows them to you
+- Several games like this are played
+
+Part 1
+------
+
+- You have to determine which games would have been possible if there were only
+  only 12 red cubes, 13 green cubes, and 14 blue cubes in the bag
+- For example, if the elf showed you a draw with 13 red cubes, that game would
+  not be possible
+- Add IDs of these games to get the answer
---
+++ 2023/D02/cube_conundrum.py
@@ -0,0 +1,77 @@
+import sys
+import re
+
+DAY = "02"
+
+
+def parse_game_line(line):
+    """parse input line that looks like this:
+    Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    """
+    game_id_txt, game_txt = re.split(r"\s*:\s*", line)
+    _, game_id = re.split(r"\s+", game_id_txt)
+    game_id = int(game_id)
+    draws_txt_lst = re.split(r"\s*;\s*", game_txt)
+
+    game = []
+    for d in draws_txt_lst:
+        cube_set_t = re.split(r"\s*,\s*", d)  # ['3 blue', '4 red']
+        draw = {}
+        for cs_t in cube_set_t:
+            count, colour = re.split(r"\s+", cs_t)  # ['3', 'blue']
+            count = int(count)
+            draw[colour] = count
+        game.append(draw)
+    return game_id, game
+
+
+def max_game_size(game, colour):
+    """max number of cube of given colour seen in any draw of the given game"""
+    max_size = 0
+    for d in game:
+        size = d.get(colour, 0)
+        if size > max_size:
+            max_size = size
+    return max_size
+
+
+def part_1_solution(lines):
+    possible_game_ids = set()
+    for line in lines:
+        game_id, game = parse_game_line(line)
+        # only 12 red cubes, 13 green cubes, and 14 blue cubes
+        possible = True
+        for max_size, colour in [(12, "red"), (13, "green"), (14, "blue")]:
+            if max_size < max_game_size(game, colour):
+                possible = False
+                break
+        if possible:
+            possible_game_ids.add(game_id)
+    # print(possible_game_ids)
+    return sum(possible_game_ids)
+
+
+def main(input_file, part=1):
+    with open(input_file) as f:
+        lines = [line.rstrip() for line in f.readlines()]
+
+    if part == 1:
+        answer = part_1_solution(lines)
+    else:
+        raise RuntimeError("part 2 not implemented")
+
+    print(f"D{DAY} part{part} answer:", answer)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print(f"usage: python {sys.argv[0]} INPUT_FILE_NAME PART_NO")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    part = sys.argv[2]
+    if part not in ["1", "2"]:
+        print(f"invalid part value '{sys.argv[2]}' must be '1' or '2'")
+        sys.exit(1)
+    part = int(part)
+    main(input_file, part)
