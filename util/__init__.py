37618ce2c384cd5bef44521bd3404c577f155e45
igzym <igor.milos@gmail.com>
2023-12-06 00:23:45 +0100

    add read_lines utility

--- util/__init__.py
+++ util/__init__.py
@@ -1,6 +1,14 @@
 import re
 
 
+def read_lines(input_file):
+    """read lines from a file and return as a list, stripping
+    trailing newlines"""
+    with open(input_file) as f:
+        lines = [line.rstrip() for line in f.readlines()]
+    return lines
+
+
 def split_re(text, sep):
     """split using given separator ignoring any white space around it"""
     return re.split(r"\s*" + sep + r"\s*", text)
