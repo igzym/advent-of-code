737f8be960d39d0074e3ac486539f5aaa06653fa
igzym <igor.milos@gmail.com>
2023-12-06 01:20:23 +0100

    D03 part 1 done
    my big problem was that I didn't account for repetition of
    numbers in the same row so simple row.find(number) did not
    work to find the starting index of a number
    
    had to iterate and do re search to solve it

--- 2023/D03/gear_ratios.py
+++ 2023/D03/gear_ratios.py
@@ -1,16 +1,84 @@
 import sys
+import re
+import util
 
 DAY = "03"
 
 
-def parse_game_line(line):
-    pass
+def read_schematic(lines):
+    schematic = []
+    for line in lines:
+        schematic.append(line)
+        print(line)
+    return schematic
+
+
+def char_at(schematic, i, j):
+    if i < 0 or i >= len(schematic):
+        return None
+    if j < 0 or j >= len(schematic[i]):
+        return None
+    return schematic[i][j]
+
+
+def is_symbol(schematic, i, j):
+    c = char_at(schematic, i, j)
+    if c is None or c == ".":
+        return False
+    if ord(c) >= ord("0") and ord(c) <= ord("9"):
+        # it's a digit, not a symbol
+        return False
+    return True
+
+
+def is_next_to_symbol(schematic, i, j):
+    b = False
+    b = b or is_symbol(schematic, i - 1, j - 1)
+    b = b or is_symbol(schematic, i - 1, j)
+    b = b or is_symbol(schematic, i - 1, j + 1)
+    b = b or is_symbol(schematic, i, j - 1)
+    b = b or is_symbol(schematic, i, j + 1)
+    b = b or is_symbol(schematic, i + 1, j - 1)
+    b = b or is_symbol(schematic, i + 1, j)
+    b = b or is_symbol(schematic, i + 1, j + 1)
+    return b
 
 
 def part_1_solution(lines):
-    for line in lines:
-        pass
-    raise RuntimeError(f"part {part} not implemented")
+    schematic = read_schematic(lines)
+
+    answer = 0
+
+    i = 0
+    for row in schematic:
+        # find all numbers and their locations
+        numbers = []
+        num_start_idx = []
+        ss = 0  # start of search
+        while True:
+            m = re.search(r"[0-9]+", row[ss:])
+            if not m:
+                break
+            b, e = m.span()
+            match = m.string[b:e]
+            numbers.append(match)
+            num_start_idx.append(ss + b)
+            ss += e  # start next search after the last match
+        print(f"row {row}", i, numbers, num_start_idx)
+        # now check the surroundings of each number
+        for ni in range(len(numbers)):
+            n = numbers[ni]
+            idx = num_start_idx[ni]
+            is_part_number = False
+            for j in range(idx, idx + len(n)):
+                if is_next_to_symbol(schematic, i, j):
+                    is_part_number = True
+                    break
+            if is_part_number:
+                print("...", n, "is a part number")
+                answer += int(n)
+        i += 1
+    return answer
 
 
 def part_2_solution(lines):
@@ -22,9 +90,8 @@ def part_2_solution(lines):
 # ===== part below doesn't change =====
 
 
-def main(input_file, part=1):
-    with open(input_file) as f:
-        lines = [line.rstrip() for line in f.readlines()]
+def main(input_file, part):
+    lines = util.read_lines(input_file)
 
     if part == 1:
         answer = part_1_solution(lines)
