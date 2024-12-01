53f653e8b4dfecc5efda287b29bab118cd6925e8
igzym <igor.milos@gmail.com>
2024-12-01 12:32:29 +0100

    refactor

--- 2024/D01/hysteria.py
+++ 2024/D01/hysteria.py
@@ -16,13 +16,15 @@ def main(lines, part):
     # create a list of pairs of integers
     values = [list(map(int, x.split())) for x in lines]
 
-    if part == 1:
-        # convert into two columns, value on the left to the first columns
-        # and the other value to the second column
-        # sor those two columns independently
-        col1 = sorted([x[0] for x in values])
-        col2 = sorted([x[1] for x in values])
+    # convert into two columns, value on the left to the first columns
+    # and the other value to the second column
+    col1 = [x[0] for x in values]
+    col2 = [x[1] for x in values]
 
+    if part == 1:
+        # sort those two columns independently
+        col1 = sorted(col1)
+        col2 = sorted(col2)
         # result is the sum of the differences of the two respective elements
         result = sum([abs(a - b) for (a, b) in zip(col1, col2)])
     else:
