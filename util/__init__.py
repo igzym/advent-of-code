ef207226514af1a412a588a222bd6949756376b0
igzym <igor.milos@gmail.com>
2024-12-01 12:10:49 +0100

    Add a reusable argument parser

--- util/__init__.py
+++ util/__init__.py
@@ -1,4 +1,5 @@
 import re
+import argparse
 
 
 def read_lines(input_file):
@@ -12,3 +13,17 @@ def read_lines(input_file):
 def split_re(text, sep):
     """split using given separator ignoring any white space around it"""
     return re.split(r"\s*" + sep + r"\s*", text)
+
+
+def parse_args(script_name, day_number):
+    parser = argparse.ArgumentParser(
+                        prog=f"python {script_name}",
+                        description=f"Solution to advent of code D{day_number:02} problem",
+                        )
+
+    parser.add_argument('filename')
+    requiredNamed = parser.add_argument_group('required named arguments')
+    requiredNamed.add_argument('-p', '--part', help='part number', type=int, choices=[1, 2], required=True)
+
+    args = parser.parse_args()
+    return args
