e072ce66d2fa033cd9165fc3c9ec12d38672fd13
igzym <igor.milos@gmail.com>
2024-12-02 08:06:33 +0100

    D02-1 sequence analysis

---
+++ 2024/D02/README.md
@@ -0,0 +1,9 @@
+D02 Ted-Nosed Reports
+======================
+
+https://adventofcode.com/2024/day/2
+
+Run as
+
+    python reports.py -part=1 input.txt
+
---
+++ 2024/D02/reports.py
@@ -0,0 +1,59 @@
+import os
+import util
+
+script_name = os.path.basename(__file__)
+day_number = 2
+
+args = util.parse_args(script_name, day_number)
+
+print(args.filename, args.part)
+
+lines = util.read_lines(args.filename)
+
+
+def sign(x):
+    if x < 0:
+        return -1
+    elif x == 0:
+        return 0
+    else:
+        return 1
+
+def is_safe(levels):
+    delta_sign_sum = 0  # sum of signs of the deltas between consecutive levels
+    unsafe_level_change = False
+    for i in range(0, len(levels) - 1):
+        delta = levels[i + 1] - levels[i]
+        if abs(delta) < 0 or abs(delta) > 3:
+            unsafe_level_change = True
+            break 
+        delta_sign_sum += sign(delta)
+
+    if unsafe_level_change:
+        return False
+    
+    if abs(delta_sign_sum) != len(levels) - 1:
+        # change signs were not monotonic
+        return False
+    
+    return True
+
+
+def main(lines, part):
+    # convert input to lists of integers
+    values = [list(map(int, x.split())) for x in lines]
+    result = 0
+
+    if part == 1:
+        for levels in values:
+            if is_safe(levels):
+                result += 1
+    else:
+        raise RuntimeError("part 2 not yet implemented")
+
+    return result
+
+
+if __name__ == "__main__":
+    result = main(lines, args.part)
+    print(f"{script_name} D{day_number:02} input file {args.filename} part {args.part} result: {result}")
