637621fd676097b1603311385278c738b0714702
igzym <igor.milos@gmail.com>
2024-12-07 14:28:58 +0100

    An attempt at D06-2 - cycle detection is too limited
    Finds 3 out of 6 possible obstacles that would create a cycle in the test
    problem

--- 2024/D06/guard.py
+++ 2024/D06/guard.py
@@ -2,7 +2,7 @@ import os
 import util
 import numpy as np
 from collections import defaultdict
-from typing import Tuple, List, Set  # need to upgrade python to 3.12 for modern typing syntax
+from typing import Tuple, List, Set, Optional  # need to upgrade python to 3.12 for modern typing syntax
 
 from util import debug
 
@@ -56,7 +56,12 @@ class Guard:
         self.initial_dir = self.dir
 
         self.visited: Set[Position] = set()  # visited positions
-        self.visited.add(self.initial_position)
+        self.visited.add(self.position)
+
+        self.visited_in_dir = defaultdict(set)  # set of direction in which given position was visited
+        self.visited_in_dir[self.position].add(self.dir)
+
+        self.additional_obstacles: Set[Position] = set()  # locations of possible additional obstacles identified so far
 
 
     def next_pos(self) -> Position:
@@ -74,8 +79,9 @@ class Guard:
         return i, j
 
 
-    def is_pos_unobstructed(self, i: int, j: int) -> bool:
+    def is_pos_unobstructed(self, position: Position) -> bool:
         # does given position contain an obstacle?
+        i, j = position
         return self.mapm[i, j] == 0
 
 
@@ -108,13 +114,16 @@ class Guard:
     def move(self) -> None:
         # change the state, either change the position if possible
         # or turn
-        i, j = self.next_pos()
-        if self.is_pos_unobstructed(i, j):
-            self.position = i, j
+        position = self.next_pos()
+        if self.is_pos_unobstructed(position):
+            self.position = position
             self.visited.add(self.position)
+            self.visited_in_dir[self.position].add(self.dir)
             debug("move to", self.position, "in dir", self.dir)
         else:
             self.turn()
+            self.visited_in_dir[self.position].add(self.dir)
+            self.print_state()
             debug("turn in", self.position, "in dir", self.dir)
 
 
@@ -126,7 +135,7 @@ class Guard:
         return f"Guard at {self.position}, dir: {self.dir}"
 
 
-    def print_state(self, visited_in_dir, iobs=None, jobs=None) -> None:
+    def print_state(self, obs_pos: Optional[Position]=None) -> None:
         m, n = self.mapm.shape
         pmtx = []
         for i in range(m):
@@ -136,8 +145,8 @@ class Guard:
                 if self.mapm[i, j] != 0:
                     pmtx[i][j] = "#"
                 else:
-                    if (i, j) in visited_in_dir:
-                        dirs = visited_in_dir[(i, j)]
+                    if (i, j) in self.visited_in_dir:
+                        dirs = self.visited_in_dir[(i, j)]
                         vert = Guard.UP in dirs or Guard.DOWN in dirs
                         hori = Guard.LEFT in dirs or Guard.RIGHT in dirs
                         if vert and hori:
@@ -149,14 +158,13 @@ class Guard:
                         else:
                             pmtx[i][j] = "?"
         i0, j0 = self.initial_position
-        pmtx[si0][j0] = self.initial_dir
-        if iobs is not None:
+        pmtx[i0][j0] = self.initial_dir
+        if obs_pos is not None:
+            iobs, jobs = obs_pos
             pmtx[iobs][jobs] = "O"
 
         for r in pmtx:
             debug("".join(r))
-
-
 
 
 def main(lines, part):
@@ -167,9 +175,6 @@ def main(lines, part):
     debug(guard.mapm)
     debug("guard state", guard)
 
-    visited_in_dir = defaultdict(set)  # set of direction in which given position was visited
-    additional_obstacles = set()  # locations of possible additional obstacles identified so far
-
 
     if part == 1:
         while True:
@@ -178,50 +183,47 @@ def main(lines, part):
             if guard.about_to_exit():
                 # next move, the guard will exit, stop iterating
                 result = guard.count_of_visited_positions()
-                debug(result, "exiting", guard)
+                debug("exiting", guard)
                 break
             # generate next move
             guard.move()
     else:
         # part 2
-        pass
-        """while True:
-            debug(result, "in state", state)
-            i, j, d = state
+        while True:
+            debug(result, "in state", guard)
 
-            if about_to_exit(mapm, state):
-                debug("about to exit", state)
+            if guard.about_to_exit():
+                debug(result, "about to exit", guard)
                 break
 
             # have I been on this location in the past
             # but going in the direction I would go if I turned
             # right?
-            past_dirs = visited_in_dir[(i, j)]
+            past_dirs = guard.visited_in_dir[guard.position]
             if past_dirs:
-                debug("seen", (i, j), "in directions", past_dirs)
+                debug("seen", guard.position, "in directions", past_dirs)
 
-            visited_in_dir[(i, j)].add(d)  # mark location as visited in current direction
-
-            nd = dir_after_turn(d)
+            nd = guard.new_direction()
             # was I in this state but in "next turn" direction?
             if nd in past_dirs:
                 debug(f"... was in direction after turn, '{nd}', in this position alrady")
-                ni, nj = next_pos(state)
-                if (ni, nj) == (i0, j0):
-                    continue  # we cannot place an obstruction on guards initial location
-                if not is_pos_unobstructed(mapm, ni, nj):
-                    continue  # there is already an obstruction
-                if (ni, nj) in additional_obstacles:
+                # see if we generate a cycle by placing an obstcle just in front of the
+                # current guard position
+                obstacle_position = guard.next_pos()
+                if obstacle_position  == guard.initial_position:
+                    continue  # we cannot place an obstacle on guards initial location
+                if not guard.is_pos_unobstructed(obstacle_position):
+                    continue  # there is already an obstacle
+                if obstacle_position in guard.additional_obstacles:
                     continue  # we have already considered and addition of an obstacle here
-                # looks like we identified a potential new obstruction
-                additional_obstacles.add((ni, nj))
+                # looks like we identified a potential new obstacle
+                guard.additional_obstacles.add(obstacle_position)
                 result += 1
-                debug(result, "potential new obstruction", (ni, nj), additional_obstacles)
-                print_state(mapm, i0, j0, visited_in_dir, ni, nj)
+                debug(result, "potential new obstacle", obstacle_position, guard.additional_obstacles)
+                guard.print_state(obstacle_position)
                 ## XXX next idea: was in direction after turn on the same line...
             # generate next move
-            move(mapm, state)
-        pass"""
+            guard.move()
 
     return result
 
