b221f8dc4888e0fb00777ea9c28d34cd0ed36fb9
igzym <igor.milos@gmail.com>
2024-12-08 19:34:47 +0100

    2024 D06-2 map modification to include cycles

--- 2024/D06/README.md
+++ 2024/D06/README.md
@@ -12,8 +12,15 @@ Run as
 part 1 path tracing on a map
 part 2 path tracing and identifying loop possibilities
 
+The second part took way more time than expected. I was trying for some fancy solution of starting a simulated
+walk from the current position when retracing the actual walk to find the cycles. But instead I should have
+just restarted from the initial position and adding the potential obstacles one by one to face the states visited
+in the actual walk (part1), and detecting if the walk in this new map ends off map or cycling.
+
+Otherwise I was getting too many solutions.
+
 
 ## Answers
 
 - part 1: 5312
-- part 2: 
+- part 2: 1748
--- 2024/D06/guard.py
+++ 2024/D06/guard.py
@@ -18,7 +18,7 @@ lines = util.read_lines(args.filename)
 
 
 Position = Tuple[int, int]
-
+State = Tuple[Position, str]
 
 class Guard:
     UP: str = "^"
@@ -27,15 +27,17 @@ class Guard:
     RIGHT: str = ">"
 
     mapm = None
+    initial_position: Position = None
+    initial_dir = None
 
     # directions in the order of turning to the right
     dir_cycle = [UP, RIGHT, DOWN, LEFT]
 
 
-    def __init__(self, lines: List[str]) -> None:
+    def initialize_map(lines: List[str]) -> None:  # NOTE: class method
         if not lines:
             raise ValueError("empty data set on input")
-        
+
         lines_mtx = util.to_matrix(lines)
         m = len(lines_mtx)
         n = len(lines_mtx[0])
@@ -43,29 +45,27 @@ class Guard:
         # the map of the area, indicating empty spaces and obstacles
         Guard.mapm = np.zeros(shape=(m, n), dtype=int)
 
-        self.position: Position = None  # current positon of the guard
-        self.dir: str = None  # current direction of the guard
-
         for i in range(m):
             for j in range(n):
                 c = lines_mtx[i][j]
                 if c == "#":
-                    self.mapm[i, j] = 1  # obstacle in map
+                    Guard.mapm[i, j] = 1  # obstacle in map
                 elif c in Guard.dir_cycle:
-                    self.position: Position = i, j  # guard position
-                    self.dir = c
-        # save guard's starting position and direction
-        self.initial_position: Position = self.position
-        self.initial_dir = self.dir
+                    Guard.initial_position = i, j
+                    Guard.initial_dir = c
 
-        self.visited: Set[Position] = set()  # visited positions
-        self.visited.add(self.position)
 
-        self.visited_in_dir = defaultdict(set)  # set of direction in which given position was visited
-        self.visited_in_dir[self.position].add(self.dir)
+    def __init__(self, state: Optional[State]=None) -> None:
+        if Guard.mapm is None:
+            raise AssertionError("class variables have not been initialized")
 
-        self.additional_obstacles: Set[Position] = set()  # locations of possible additional obstacles identified so far
+        if state is not None:
+            self.position, self.dir = state
+        else:
+            self.position = Guard.initial_position
+            self.dir = Guard.initial_dir
 
+        self.extra_obstacle: Position = None
 
     def next_pos(self) -> Position:
         # calculate next position if we were to move
@@ -84,8 +84,14 @@ class Guard:
 
     def is_pos_unobstructed(self, position: Position) -> bool:
         # does given position contain an obstacle?
+        if position == self.extra_obstacle:  # we play with extra obstacles in part 2
+            return False
         i, j = position
         return self.mapm[i, j] == 0
+    
+
+    def set_extra_obstacle(self, position: Position) -> bool:
+        self.extra_obstacle = position
 
 
     def is_off_map(self, i: int, j: int) -> bool:
@@ -124,14 +130,9 @@ class Guard:
         position = self.next_pos()
         if self.is_pos_unobstructed(position):
             self.position = position
-            self.visited.add(self.position)
-            self.visited_in_dir[self.position].add(self.dir)
-            #debug("move to", self.position, "in dir", self.dir)
         else:
             self.turn()
-            self.visited_in_dir[self.position].add(self.dir)
-            self.print_state()
-            #debug("turn in", self.position, "in dir", self.dir)
+        assert self.is_pos_unobstructed(self.position)
 
 
     def count_of_visited_positions(self) -> int:
@@ -142,105 +143,96 @@ class Guard:
         return f"Guard at {self.position}, dir: {self.dir}"
 
 
-    def print_state(self, obs_pos: Optional[Position]=None) -> None:
-        return
-        m, n = self.mapm.shape
-        pmtx = []
-        for i in range(m):
-            pmtx.append(["."] * n)
-        for i in range(m):
-            for j in range(n):
-                if self.mapm[i, j] != 0:
-                    pmtx[i][j] = "#"
-                else:
-                    if (i, j) in self.visited_in_dir:
-                        dirs = self.visited_in_dir[(i, j)]
-                        vert = Guard.UP in dirs or Guard.DOWN in dirs
-                        hori = Guard.LEFT in dirs or Guard.RIGHT in dirs
-                        if vert and hori:
-                            pmtx[i][j] = '+'
-                        elif vert:
-                            pmtx[i][j] = "|"
-                        elif hori:
-                            pmtx[i][j] = "-"
-                        else:
-                            pmtx[i][j] = "?"
-        i0, j0 = self.initial_position
-        pmtx[i0][j0] = self.initial_dir
-        if obs_pos is not None:
-            iobs, jobs = obs_pos
-            pmtx[iobs][jobs] = "O"
-
-        for r in pmtx:
-            debug("".join(r))
-
-
 def main(lines, part):
     result = 0
 
-    guard = Guard(lines)
+    Guard.initialize_map(lines)
+    guard = Guard()
     debug("map")        
     debug(guard.mapm)
     debug("guard state", guard)
 
+    # let the guard follow its path until it leaves the map
+    # unique positions visited (part 1 question)
+    visited_positions: Set[Position] = set()
+    path_trace: List[Position] = [] # sequential list of positons, not necessarily unique, for debugging
+    # register all the states (position and direction) it passes through (needed for part 2)
+    visited_states: Set[State] = set()  # unique visited states
+    visited_states_list: List[State] = []  # unique visited states in order of visit
+    while True:
+        debug("part 1: guard in positon", guard)
+        visited_positions.add(guard.position)
+        path_trace.append(guard.position)
+        state = guard.position, guard.dir
+        if state not in visited_states:
+            visited_states.add(state)
+            visited_states_list.append(state)
+        if guard.about_to_exit():
+            # next move, the guard will exit, stop iterating
+            #debug("guard about to leave the grid, stop iteration", guard)
+            print("part 1: leaving map in state", guard)
+            break
+        guard.move()
+
 
     if part == 1:
-        while True:
-            #debug(result, "in state", guard)
-            i, j = guard.position
-            if guard.about_to_exit():
-                # next move, the guard will exit, stop iterating
-                result = guard.count_of_visited_positions()
-                debug("exiting", guard)
-                break
-            # generate next move
-            guard.move()
+        result = len(visited_positions)
     else:
         # part 2
-        # look for potential obstacles to add to introduce cycles
-        while True:
-            #debug(result, "in state", guard)
+        # for each state (position and direction) the guard visits, place an extra obstacle
+        # just in front and let the guard run until it leaves the map or cycles
+        # if a cycle is detected (revisiting the same state), the obstacle is added to the set
+        # of solutions
 
-            if guard.about_to_exit():
-                debug(result, "about to exit the grid", guard)
-                break
+        state_idx = 0
+        obstacles_found: Set[Position] = set()
+        N = len(visited_states_list) - 1
+        # skip the last of the visited states because that one is just before going off the map
+        for position, dir in visited_states_list[:-1]:
 
-            if guard.is_facing_obstacle():
-                #debug(result, "already facing an obstacle", guard)
-                guard.move()
+            state_idx += 1
+            guard_for_obstacle_sim = Guard((position, dir))
+            debug("guard for obstacle simulation", guard_for_obstacle_sim)
+
+            # this can happen only on the last visited state, which we excluded from the loop
+            assert not guard_for_obstacle_sim.about_to_exit()
+
+            if guard_for_obstacle_sim.is_facing_obstacle():
+                # already facing an obstacle
+                debug("this state is already facing an obstacle, stop", guard_for_obstacle_sim)
                 continue
 
-            # consider placing an obstacle in the next positon the guard will take 
-            obstacle_position = guard.next_pos()
+            obstacle_position = guard_for_obstacle_sim.next_pos()
+            debug("assuming obstacle at", obstacle_position)
+            if obstacle_position == Guard.initial_position:
+                debug(f"cannot place obstacle at {obstacle_position} because it's the guard's initial position")
+                continue
+            if obstacle_position in obstacles_found:
+                debug(f"obstacle {obstacle_position} already identified as cycle-inducing")
+                continue
 
-            if obstacle_position in guard.additional_obstacles:
-                guard.move()
-                continue  # we have already identified this as a cycle-inducing obstacle position
-
-            if obstacle_position == guard.initial_position:
-                guard.move()
-                continue  # we cannot place an obstacle on guards initial location
-
-            # imagine there was an obstacle in front and the gard turned and continued in
-            # that direction. Would he cross a previouslly visited position going in that direction?
-            # if so, placing an obstcle would induce a cycle
-            sim_guard = copy.deepcopy(guard)
-            sim_guard.turn()
+            # let the guard run with the extra obstacle in place
+            # until exit or until it returns to the initial state
+            guard = Guard()
+            guard.set_extra_obstacle(obstacle_position)
+            # visited_states_2: Set[State] = set()
+            # then iterate until you get off the grid or cycle
+            cycle_len = 0
+            visited_states_2: Set[State] = set()
             while True:
-                if sim_guard.about_to_exit() or sim_guard.is_facing_obstacle():
+                debug("... inner guard", guard)
+                if guard.about_to_exit():
+                    debug("... simulated guard going off map, stop", guard)
                     break
-                past_guard_dirs_at_sim_position = guard.visited_in_dir.get(sim_guard.position)
-                if past_guard_dirs_at_sim_position is not None:
-                    if sim_guard.dir in past_guard_dirs_at_sim_position:
-                        # guard has already visited the present position and in the
-                        # same direction, so the obstacle would create a cycle
-                        guard.additional_obstacles.add(obstacle_position)
-                        result += 1
-                        debug(result, f"({len(guard.visited)}) cycle-inducing obstacle at", obstacle_position)
-                        guard.print_state(obstacle_position)
-                        break
-                sim_guard.move()
-            guard.move()
+                state = guard.position, guard.dir
+                if state in visited_states_2:
+                    result += 1
+                    debug(f"{result:4}: ({state_idx}/{N}) FOUND cycle for {obstacle_position} of length {cycle_len}")
+                    obstacles_found.add(obstacle_position)
+                    break
+                visited_states_2.add(state)
+                guard.move()
+                cycle_len += 1
 
     return result
 
